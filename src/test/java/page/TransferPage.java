1993ea2612251ce18a9d4658660a315fe2c48200
Victoriya Fadeeva <vic.f4deewa@gmail.com>
2025-11-27 12:19:18 +0300

    внесены изменения в TransferPage

--- src/test/java/page/TransferPage.java
+++ src/test/java/page/TransferPage.java
@@ -1,26 +1,40 @@
 package page;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import data.DataHelper;
 
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 
 public class TransferPage {
-    private SelenideElement amountField = $("[data-test-id='amount'] input");
-    private SelenideElement fromCardField = $("[data-test-id='from'] input");
-    private SelenideElement transferButton = $("[data-test-id='action-transfer']");
+    private final SelenideElement transferButton = $("[data-test-id='action-transfer']");
+    private final SelenideElement amountInput = $("[data-test-id='amount'] input");
+    private final SelenideElement fromInput = $("[data-test-id='from'] input");
+    private final SelenideElement transferHead = $(byText("Пополнение карты"));
+    private final SelenideElement errorMassage = $("[data-test-id='error-notification'] .notification__content");
 
-    public TransferPage setAmount(int amount) {
-        amountField.clear();
-        amountField.setValue(String.valueOf(amount));
-        return this;
+    public TransferPage() {
+        transferHead.shouldBe(visible);
     }
-    public TransferPage setFromCard(String cardNumber) {
-        fromCardField.clear();
-        fromCardField.setValue(cardNumber);
-        return this;
+
+
+    public DashBoardPage makeValidTransfer(String amountToTransfer, DataHelper.CardInfo cardInfo) {
+        makeTransfer(amountToTransfer, cardInfo);
+        return new DashBoardPage();
     }
-    public void clickTransfer() {
+
+    public void makeTransfer(String amountToTransfer, DataHelper.CardInfo cardInfo){
+        amountInput.setValue(amountToTransfer);
+        fromInput.setValue(cardInfo.getCardNumber());
         transferButton.click();
+    }
+
+    public void findErrorMassage(String expectedText) {
+        errorMassage.shouldBe(visible, Duration.ofSeconds(15)).shouldBe(Condition.text(expectedText));
     }
 
 }
